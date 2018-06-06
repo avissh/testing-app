712870bd57a7ad614605789fa5572938bacd89e3
glearner <gkvom@outlook.com>
2018-06-07 02:06:16 +0530

    fixed minor bugs

--- devServer.js
+++ devServer.js
@@ -6,7 +6,7 @@ const config = require('./webpack.config.dev')
 const sgMail = require('@sendgrid/mail');
 
 var app = express();
-const apiKey = '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';//API KEY VALID for sometime only
+const apiKey = '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';//API KEY VALID for sometime only
 sgMail.setApiKey(apiKey);
 app.use(bodyParser.json());
 
@@ -24,8 +24,8 @@ app.use(require('webpack-hot-middleware')(compiler))
 
 app.post('/send_mail',(req,res)=>{
   const msg = {
-    to: req.body.email,
-    from: 'gkverma1094@gmail.com',//Please provide ur email-id here :}
+    to: req.body.email,//Please provide ur email-id here :}
+    from: 'gkverma1094@gmail.com',
     subject: req.body.subject,
     text: req.body.message,
     html: `<strong>${req.body.message}</strong>`,
